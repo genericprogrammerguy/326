a2918704bb09a96c872385964d1ecde669ae748a
walst388 <stefancraig@hotmail.com>
2020-02-01 17:12:59 +1300

    updated gui.py file with arbitrary drawing line class

--- 04/hilbert.py
+++ 04/hilbert.py
@@ -1,31 +1,58 @@
-import sys, math
+from turtle import *
 
-def hilbert_curve(x, y, xi, xj, yi, yj, n):
+# the first 2 parameters x and y coordinates are used in the calculation of a point on the hb curve
+# the next 4 parameters  xi, xj ,yi ,yj define the two vectors this helps moves the point proportional to x,y vector
+#
+#            D----------------C
+#                             |
+#                             |
+#                             |
+#                             |
+#                             |
+#                             |
+#            A----------------B
+#
+#          Order1
+# n is the order of magnitude
+
+def hilbert_curve(turtle: object, x: object, y: object, xi, xj, yi, yj, n: object) -> object:
+
     # recursive call
     if n <= 0:
-        X = (x + (xi + yi)/2)
-        Y = (y + (xj + yj)/2)
-
-        print('%s %s 0' % (X,Y))
+        return
+        x = (x + (xi + yi)/2)
+        y = (y + (xj + yj)/2)
+    # print('%s %s 0' % (X,Y))
     else:
-        hilbert_curve(x,                    y,                   yi/2, yj/2, xi/2, xj/2, n - 1)
-        hilbert_curve(x + xi/2,             y + xj/2,            xi/2, xj/2, yi/2, yj/2, n - 1)
-        hilbert_curve(x + xi/2 + yi/2,      y + xj/2 + yj/2,     xi/2, xj/2, yi/2, yj/2, n - 1) 
-        hilbert_curve(x + xi/2 + yi,        y + xj/2 + yj,       -yi/2,-yj/2,-xi/2, -xj/2, n - 1)
-        
+        #turtle.left(y * 90)
+        # Point A
+        turtle.left.hilbert_curve(x, y, yi/2, yj/2, xi/2, xj/2, n - 1)
 
-def main():
-    arg = sys.argv
-    pixels = float(arg[1])
-    ctype = arg[2]
-    reps = int(arg[3])
-    width = float(arg[4])
+        turtle.foward(x)
 
-    curves = int(math.pow(4, reps))
+        #turtle.right(y * 90)
+        # Point B
+        turtle.right.hilbert_curve(x + xi/2, y + xj/2, xi/2, xj/2, yi/2, yj/2, n - 1)
 
-    hilbert_curve(0.0, 0.0, 1.0, 0.0, 1.0, 0.0, reps)
+        turtle.foward(x)
 
-#    print('] \"constanatwidth\" [%s]' % width)
+        # turtle.right(y * 90)
+        # Point C
+        turlte.right.hilbert_curve(x + xi/2 + yi/2, y + xj/2 + yj/2, xi/2, xj/2, yi/2, yj/2, n - 1)
 
-if __name__ == "__main__":
-    main()
+        turtle.foward(x)
+        # Point D
+        hilbert_curve(x + xi/2 + yi, y + xj/2 + yj, -yi/2,-yj/2,-xi/2, -xj/2, n - 1)
+        #turlte.left(x * 90)
+
+if __name__ == "___main___":
+
+    s = Screen()
+
+    t = Turtle()
+    t.shape('turtle')
+    t.speed('slow')
+    n = 3
+
+    hilbert_curve(x, y ,xi, xj, yi, yj, n)
+
