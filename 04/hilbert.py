2e0f5a285e2a726c2bbca2a4d8c06b8624ef8a15
walst388 <stefancraig@hotmail.com>
2020-02-12 01:22:59 +1300

    need to finsh resizing of hilbert curve

--- 04/hilbert.py
+++ 04/hilbert.py
@@ -1,7 +1,6 @@
 from turtle import Screen, Turtle
 
-# the first 2 parameters x and y coordinates are used in the calculation of a point on the hb curve
-# the next 4 parameters  xi, xj ,yi ,yj define the two vectors this helps moves the point proportional to x,y vector
+
 #
 #            D----------------C
 #                             |
@@ -15,54 +14,59 @@ from turtle import Screen, Turtle
 #          Order1
 # n is the order of magnitude
 
-# def hilbert_curve(t,x, y, xi, xj, yi, yj, n):
-#     # recursive call
-#     if n < 1:
-#         return
-#         x = x + (xi + yi)/2
-#         print("X")
-#         y = y + (xj + yj)/2
-#         print("Y")
-#     else:
-#         # Point A
-#         hilbert_curve(t,    x,      y,      yi/2,   yj/2,   xi/2,   xj/2,   n - 1)
-#         t.forward(10)
-#         print("pointA")
-#         # Point B
-#         t.left(x * 90)
-#         hilbert_curve(t,    x +     xi/2,   y +     xj/2,   xi/2,   xj/2,   yi/2, yj/2, n - 1)
-#         t.forward(10)
-#         print("pointB")
-#         # Point C
-#         t.left(y * 90)
-#         hilbert_curve(t,    x +     xi/2 +  yi/2,   y +     xj/2 +  yj/2,   xi/2, xj/2, yi/2, yj/2, n - 1)
-#         print("pointC")
-#         t.forward(10)
-#         # Point D
-#         t.left(x * 90)
-#         hilbert_curve(t,    x +     xi/2 +  yi,     y +     xj/2 +  yj,     -yi/2,-yj/2,-xi/2, -xj/2, n - 1)
-#         print("pointD")
+order = 6
 
+def hilbert(t,dir, rot, order):
+    if( order == 0):
+        return
 
+    # POINT A
+    #dir = dir - rot
+    t.right(rot * 90)
+    print("(R)")
+    hilbert(t, dir, - rot, order - 1)
+    t.forward(dir)
+    print("F")
+    
+    # POINT B
+    #dir = dir - rot
+    t.left(rot * 90)
+    print("((L))")
+    hilbert(t, dir,  rot, order - 1)
+    t.forward(dir)
+    print("F")
 
-def hilbert(dir, rot, order):
+    #POINT C
+    #dir = dir - rot
+    print("(((L)))")
+    hilbert(t, dir,  rot, order - 1)
+    t.left(rot * 90)
+    t.forward(dir)
+    print("F")
 
-
+    # POINT D
+    #dir = dir - rot
+    hilbert(t, dir, - rot, order - 1)
+    t.right(rot * 90)
+    print("((((R))))")
 
 def rescale():
-    turtle.reset()
-    screen.setworlscoordinates()
-    hilbert_curve()
-    screen.update()
-    screen.ontimer(rescale)
-    pass
-    
+    t.reset()
+    t.speed("fastest")
+    s.setworlscoordinates(0,0,2**order-1,2**order-1)
+    hilbert(t, length, rot, order)
+    s.update()
+    s.ontimer(rescale, 1000)
 
-if __name__ =="__main__":
+def main():
+    length = 100/(4*order-1)
     s = Screen()
+    s.tracer(False)
     t = Turtle()
 
-    herbert(1,2,3)
-    # hilbert_curve(t,0.0, 0.0 ,1.0, 0.0, 0.0, 1.0, 4)
+    hilbert(t, 10, 1, order)
 
     s.mainloop()
+
+if __name__ =="__main__":
+    main()
