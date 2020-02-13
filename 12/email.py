5d43600597b5ba12b16566c0015993d18089a599
walst388 <stefancraig@hotmail.com>
2020-02-14 09:27:58 +1300

    email WIP

--- 12/email.py
+++ 12/email.py
@@ -8,26 +8,24 @@ import socket
 # A domain name
 # A dot(".")
 # A domain extension
-# Mailbox and domain names must alphanumeric
-# multiple parts separated by single dots (".").
-# Parts of the mailbox name may be similarly separated by single hyphens ( "-" ) and/or underscores ("_")
-# "_dot_" should be "."
-#  "_at_" should be "@"
-#
-# Domain extension must be one of the following:
-# co.nz
-# com.au
-# co.ca
-# co.uk
-# com
-# co.us
-#
-# Alternatively, the domain may be given in numerical form, in which case it must be surrounded by square brackets.
 
+#domain = thisdict{ "co.nz"  "com.au" , "co.ca" , "co.uk" , "com" , "co.us"}
 
-def split_email():
-    mailbox=re.compile(r'^\w+@|\w_at_')
-    domain = re.compile(r'')
+def split_email(email):
+    mailbox_RegEX=re.compile(r'^\w+@')
+    if(mailbox_RegEX.match == mailbox):
+        return True
+    else:
+        print(' <- Invalid: not a correct mailbox')
+        return False
+
+    domain_RegEx = re.compile(r'(co.nz|com.au|co.ca|co.uk|com|co.us)')
+    if (domain_RegEX.match == domain):
+        return True
+    else:
+        print(' <- Invalid: not a correct domain')
+        return False
+
     #extension =re.split()
 
 
@@ -36,21 +34,21 @@ def reformat(email):
         email.replace('_at_', '@')
         email.replace('_dot_', '.')
         if(email != email.lower):
-            email = email.lower()
+            email = email.lower
             return email
 
-def format(email):
+def check_format(email):
     if len(email) < 1:
-        print(' <- Mailbox name is too short')
+        print(' <- Invalid: mailbox name is too short')
         return False
     elif(email != email.isalnum):
-        print(' <- not alphanumeric')
+        print(' <- Invalid: not alphanumeric')
         return False
     elif(email =='@@'):
-        print(' <- Invalid format')
+        print(' <- Invalid: format')
         return False
     elif(email ==  '-_'):
-        print(' <- consecutive separators')
+        print(' <- Invalid: consecutive separators')
         return False
     # elif (email.split('.'):
     #     print(' <- Invalid mailbox name')
@@ -58,35 +56,61 @@ def format(email):
 
 def checkmail_name(email):
     pass
-    pass
 
-
-def check_domain():
-    pass
+def checkIP(ip):
+    ips = ip.split('.')
+    if len(ips) != 4:
+        print('<- Invalid: IP must be IPv4')
+        return False
+    for i in ips:
+        i = int(i)
+        if i > 256:
+            print('<- Invalid: IP address < 256')
+            return False
+        else:
+            print('<- Invalid: IP must be numerical')
+            return False
 
 def check__extension():
     pass
-
-def switcher(domain):
-    domain == {
-    1: "co.nz",
-    2: "com.au",
-    3: "co.ca",
-    4: "co.uk",
-    5: "com",
-    6: "co.us"
-    }
-    return domain
+    # if email ==domain {
+    # 1: "co.nz",
+    # 2: "com.au",
+    # 3: "co.ca",
+    # 4: "co.uk",
+    # 5: "com",
+    # 6: "co.us"
+    # }
+    # return domain
+    # else:
+    # print('<- wrong domain')
 
 def main():
-    while True:
-        email = input("Enter email:")
-        if email[0] == '  ':
-            print("Starts with white space")
-            while format(True):
-                
-                print(email)
-                return
+    # while True:
+    # email = input("Enter email:")
+    email = []
+    #print("Enter email:")
+    if sys.stdin.isatty():
+        while True:
+            email = raw_input('Enter email to test: ')
+            data.append(email)
+            # reads from file for testing:
+            for line in sys.stdin:
+                line.replace('\n', '')
+                data.append(email)
+                #for email in sys.stdin:
+                #email.replace('\n', '')
+                if email[0] == '  ':
+                    print("Starts with white space")
+                    # while format(True):
+                    email = email.strip('\n')
+                    print(email)
+                    check_format(email)
+                    print(email)
+                    reformat(email)
+                     #checkIP()
+                    print(email)
+                    break
 
 if __name__ =='__main__':
     main()
